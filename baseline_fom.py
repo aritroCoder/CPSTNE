12519a46ff1c222f54fbbf091dee916e779582ce
sujit880 <sujitgopal880@gmail.com>
2024-09-15 16:17:40 +0530

    updated sencor dataset loading

--- baseline_fom.py
+++ baseline_fom.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -10,6 +11,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm, trange
 from sklearn.preprocessing import StandardScaler
 from torch.optim.lr_scheduler import LambdaLR
+import learn2learn as l2l
 
 # set the random seed for reproducibility
 torch.manual_seed(0)
@@ -21,7 +23,9 @@ hidden_dim = 64
 basis_function_dim = 32
 output_dim = 1  # For example, z as output
 learning_rate = 2e-5
-num_epochs = 1000
+num_adapt_epochs = 5
+num_train_epochs = 1000
+few_shot_k = 4
 l1_lambda = 0.5
 l2_lambda = 0.05
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
@@ -36,37 +40,51 @@ class FOMDataset(Dataset):
         X = self.data[['thickness', 'wavelength']].values
         y = self.data['fom'].values
 
-        # divide X and y into sets of 9, each set representing a type of sensor
-        X = X.reshape(-1, 9, 2)
-        y = y.reshape(-1, 9)
-
-        if train:
-            self.X = X[:, : self.few_k_shot, :].reshape(-1, 2)
-            self.y = y[:, : self.few_k_shot].reshape(-1)
-        else:
-            self.X = X[:, self.few_k_shot:, :].reshape(-1, 2)
-            self.y = y[:, self.few_k_shot:].reshape(-1)
-        
-        self.scaler_X = StandardScaler()
-        self.X = self.scaler_X.fit_transform(self.X)
+        self.scaler_x = StandardScaler()
+        self.X = self.scaler_x.fit_transform(X)
 
         self.scaler_y = StandardScaler()
-        self.y = self.scaler_y.fit_transform(self.y.reshape(-1, 1)).reshape(-1)
+        self.y = self.scaler_y.fit_transform(y.reshape(-1, 1))
+
+        # divide X and y into sets of 9, each set representing a type of sensor
+        self.X = self.X.reshape(-1, 9, 2)
+        self.y = self.y.reshape(-1, 9)
+
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=5)
+
+        if train:
+            self.train_x = X_train[:, : self.few_k_shot, :]
+            self.train_y = y_train[:, : self.few_k_shot]
+            self.test_x = X_train[:, self.few_k_shot:, :]
+            self.test_y = y_train[:, self.few_k_shot:]
+        else:
+            self.train_x = X_test[:, : self.few_k_shot, :]
+            self.train_y = y_test[:, : self.few_k_shot]
+            self.test_x = X_test[:, self.few_k_shot :, :]
+            self.test_y = y_test[:, self.few_k_shot :]
+        
+        print(f"Train = {train}. Data loaded. Train X: {self.train_x.shape}, Train Y: {self.train_y.shape}, Test X: {self.test_x.shape}, Test Y: {self.test_y.shape}")
+
     def __len__(self):
         # Return the total number of samples
-        return len(self.X)
+        return self.train_x.shape[0]
 
     def __getitem__(self, idx):
         # Get the input features and target for a given index
-        x = torch.tensor(self.X[idx], dtype=torch.float32)
-        y = torch.tensor(self.y[idx], dtype=torch.float32).unsqueeze(0)
-        return x, y
+        context_x = torch.tensor(self.train_x[idx], dtype=torch.float32)
+        context_y = torch.tensor(self.train_y[idx], dtype=torch.float32)
+        target_x = torch.tensor(self.test_x[idx], dtype=torch.float32)
+        target_y = torch.tensor(self.test_y[idx], dtype=torch.float32)
+        return context_x, context_y, target_x, target_y
     
     def scale_inverse(self, y):
         return self.scaler_y.inverse_transform(y)
+    
+    def scale_inverse_x(self, x):
+        return self.scaler_x.inverse_transform(x)
 
-fom_dataset_train = FOMDataset(csv_file="fom.csv", train=True)
-fom_dataset_test = FOMDataset(csv_file="fom.csv", train=False)
+fom_dataset_train = FOMDataset(csv_file="fom.csv", train=True, few_k_shot=few_shot_k)
+fom_dataset_test = FOMDataset(csv_file="fom.csv", train=False, few_k_shot=few_shot_k)
 
 fom_dataloader_train = DataLoader(
     fom_dataset_train, batch_size=batch_size, shuffle=True, num_workers=16
@@ -173,14 +191,6 @@ def custom_loss_function(predictions, targets, weights, l1_lambda=0.001, l2_lamb
     l2_loss = l2_lambda * torch.norm(weights, p=2)
     return mse_loss + l1_loss + l2_loss
 
-# Lambda function for linear schedule
-def lr_lambda(current_step: int):
-    if current_step < warmup_steps:
-        return float(current_step) / float(max(1, warmup_steps))
-    return max(
-        0.0, float(total_steps - current_step) / float(max(1, total_steps - warmup_steps))
-    )
-
 model = FewShotRegressionModel(
     input_dim, hidden_dim, basis_function_dim, output_dim
 ).to(device)
@@ -188,72 +198,93 @@ model = FewShotRegressionModel(
 num_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
 print(f"Number of trainable parameters: {num_trainable_params}")
 
-model = FewShotRegressionModel(input_dim, hidden_dim, basis_function_dim, output_dim).to(device)
+maml = l2l.algorithms.MAML(model, lr=learning_rate, first_order=False, allow_unused=True)
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-total_steps = len(fom_dataloader_train) * num_epochs
+total_steps = len(fom_dataloader_train) * num_train_epochs
 warmup_steps = total_steps // 10
+
+def lr_lambda(current_step: int):
+    if current_step < warmup_steps:
+        return float(current_step) / float(max(1, warmup_steps))
+    return max(
+        0.0, float(total_steps - current_step) / float(max(1, total_steps - warmup_steps))
+    )
 scheduler = LambdaLR(optimizer, lr_lambda)
 
 torch.autograd.set_detect_anomaly(True)
 losses = []
-test_losses = []
-for epoch in trange(num_epochs):
-    model.train()
+val_losses = []
 
-    for batch_x_train, batch_y_train in fom_dataloader_train:
-        # move to device
-        batch_x_train = batch_x_train.to(device, torch.float32).view(-1, input_dim)
-        batch_y_train = batch_y_train.to(device, torch.float32).view(-1)
-        
-        batch_size = batch_x_train.shape[0]
+for epoch in range(num_train_epochs):
+    for idx, (context_x, context_y, target_x, target_y) in enumerate(fom_dataloader_train):
+        meta_train_loss = 0
+        learn_loss = 0
+        context_x, context_y, target_x, target_y = context_x.to(device), context_y.to(device), target_x.to(device), target_y.to(device)
+        effective_batch_size = context_x.size(0)
+        for i in range(effective_batch_size):
+            learner = maml.clone(first_order=True)
+            x_support, y_support = context_x[i], context_y[i]
+            x_query, y_query = target_x[i], target_y[i]
+            learn_loss = 0
+            for _ in range(num_adapt_epochs):
+                wts, predictions = learner(x_support)
+                loss = custom_loss_function(predictions, y_support, wts, l1_lambda, l2_lambda)
+                learn_loss += loss.detach().cpu().item()
+                learner.adapt(loss)
+            wts, predictions = learner(x_query)
+            loss = custom_loss_function(predictions, y_query, wts, l1_lambda, l2_lambda)
+            meta_train_loss += loss
 
-        loss = 0
-        wts, predictions = model(batch_x_train)
+        meta_train_loss /= effective_batch_size
+        learn_loss /= num_adapt_epochs*effective_batch_size
+        losses.append(learn_loss)
+        val_losses.append(meta_train_loss.item())
 
-        loss += custom_loss_function(predictions, batch_y_train, wts, l1_lambda, l2_lambda)
-
-        # Backward pass and optimization
-        loss.backward()
+        meta_train_loss.backward()
         optimizer.step()
         scheduler.step()
         optimizer.zero_grad()
+        
+    if epoch % 10 == 0:
+        print(f"Epoch: {epoch}, Train Loss: {sum(losses)/len(losses):.4f}, Val Loss: {sum(val_losses)/len(val_losses):.4f}")
 
-    # Print loss every 10 epochs
-    if (epoch+1) % 10 == 0:
-        losses.append(loss.item())
-        temp_test_losses = []
-        for batch_x_test, batch_y_test in fom_dataloader_test:
-            batch_x_test = batch_x_test.to(device, torch.float32).view(-1, input_dim)
-            batch_y_test = batch_y_test.to(device, torch.float32).view(-1)
-
-            wts, predictions = model(batch_x_test)
-            loss = custom_loss_function(predictions, batch_y_test, wts, l1_lambda, l2_lambda)
-            temp_test_losses.append(loss.item())
-        test_losses.append(sum(temp_test_losses)/len(temp_test_losses))
-        print(f'Epoch [{epoch+1}/{num_epochs}], Train Loss: {losses[-1]:.4f}, Test Loss: {test_losses[-1]:.4f}')
+    if epoch % 50 == 0:
+        torch.save(model.state_dict(), f'baseline_fom_wts.pt')
 
 # plot the losses in a graph and save as a image
 plt.plot(losses)
-plt.plot(test_losses)
+plt.plot(val_losses)
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.title('Training Loss')
 plt.legend(['Train Loss', 'Test Loss'])
-plt.savefig(f'baseline_fom_{epoch+1}.png')
+plt.savefig(f'baseline_fom.png')
 
-model.eval()
-with torch.no_grad():
-    test_losses = []
-    for batch_x_test, batch_y_test in fom_dataloader_test:
-        batch_x_test = batch_x_test.to(device, torch.float32).view(-1, input_dim)
-        batch_y_test = batch_y_test.to(device, torch.float32).view(-1)
 
-        wts, predictions = model(batch_x_test)
-        loss = custom_loss_function(predictions, batch_y_test, wts, l1_lambda, l2_lambda)
-        test_losses.append(loss.item())
-    print(f'Test Loss: {sum(test_losses)/len(test_losses):.4f}')
+test_losses = []
+for idx, (context_x, context_y, target_x, target_y) in enumerate(fom_dataloader_test):
+    meta_test_loss = 0
+    context_x, context_y, target_x, target_y = context_x.to(device), context_y.to(device), target_x.to(device), target_y.to(device)
+    effective_batch_size = context_x.size(0)
 
-    # print few test input, predicted output and actual output after inverse scaling
-    for i in range(5):
-        # print(f"Test Input: {batch_x_test[i].cpu().numpy()}, Predicted Output: {predictions[i].cpu().numpy()}, Actual Output: {batch_y_test[i].cpu().numpy()}")
-        print(f"Predicted Output (inverse scaled): {fom_dataset_test.scale_inverse(predictions.reshape(-1, 1).cpu().numpy())[i]}, Actual Output (inverse scaled): {fom_dataset_test.scale_inverse(batch_y_test.reshape(-1, 1).cpu().numpy())[i]}")
+    for i in range(effective_batch_size):
+        learner = maml.clone(first_order = True)
+        x_support, y_support = context_x[i], context_y[i]
+        x_query, y_query = target_x[i], target_y[i]
+        for i in range(num_adapt_epochs):
+            wts, predictions = learner(x_support)
+            loss = custom_loss_function(predictions, y_support, wts, l1_lambda, l2_lambda)
+            learner.adapt(loss)
+        wts, predictions = learner(x_query)
+        loss = custom_loss_function(predictions, y_query, wts, l1_lambda, l2_lambda)
+        # print few test input, predicted output and actual output
+        for i in range(2):
+            print(f"Input: {target_x[i]} Predicted Output: {fom_dataset_test.scale_inverse(predictions.reshape(-1, 1).detach().cpu().numpy())[i]}, Actual Output: {fom_dataset_test.scale_inverse(target_y.reshape(-1, 1).detach().cpu().numpy())[i]}")
+        meta_test_loss += loss
+    test_losses.append(loss.detach().cpu().item())
+    if idx % 10 == 0:
+        print(f"Step: {idx}, Test Loss: {sum(test_losses)/len(test_losses):.4f}")
+
+
+print(f'Average Test Loss: {sum(test_losses)/len(test_losses):.4f}')
+
